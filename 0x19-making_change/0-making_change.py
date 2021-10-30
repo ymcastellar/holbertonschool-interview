30718677ce620ccc5c0b76f36ae0be26dab4504e
ymcastellar <yoycas@hotmail.com>
2021-10-30 15:48:51 -0500

    all files added

---
+++ 0x19-making_change/0-making_change.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Make change module
+"""
+
+
+def makeChange(coins, total):
+    """Given a pile of coins of different values,
+    determine the fewest number of coins
+    needed to meet a given amount 'total'
+    Args:
+        coins ([list]): a list of the values of the coins in your possession
+        total ([number]): amount
+    Return: fewest number of coins needed to meet total
+    """
+    if total <= 0:
+        return 0
+
+    coins.sort(reverse=True)
+
+    i, ncoins = (0, 0)
+    cpy_total = total
+    len_coins = len(coins)
+
+    while(i < len_coins and cpy_total > 0):
+        if (cpy_total - coins[i]) >= 0:
+            cpy_total -= coins[i]
+            ncoins += 1
+        else:
+            i += 1
+
+    check = cpy_total > 0 and ncoins > 0
+    return -1 if check or ncoins == 0 else ncoins
