60b8bd29a84ea391d5883000ef9e1557847e37a7
ymcastellar <yoycas@hotmail.com>
2021-11-18 21:24:55 -0500

    adding file for task

---
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Island Perimeter
+"""
+
+
+def island_perimeter(grid):
+    """
+    island_perimeter
+    """
+    _sum, limit = (0, 0)
+    len_grid, len_col = (len(grid), len(grid[0]))
+
+    for col in range(len_grid):
+        _sum += sum(grid[col])
+        for row in range(len_col):
+            if grid[col][row]:
+                if row > 0 and grid[col][row - 1] == 1:
+                    limit += 1
+
+                if col > 0 and grid[col - 1][row] == 1:
+                    limit += 1
+    return _sum * 4 - limit * 2
